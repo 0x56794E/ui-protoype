65f19681db81209e8dfaaf8b8cd3fa340800cbff
Vy Thuy <vng0510@gmail.com>
2017-03-20 16:00:04 -0400

    add more views

--- ui/polls/views.py
+++ ui/polls/views.py
@@ -1,4 +1,28 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+
+from .models import Question
 
 def index (request):
-    return HttpResponse("Hello world!")
+    q_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'q_list' : q_list
+    }
+
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        q = Question.objects.gets(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    
+    return render(request, 'polls/details.html', {'question' : q})
+
+def results(request, question_id):
+    response = "You're looking at results of q %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on q %s." %  question_id)
+
