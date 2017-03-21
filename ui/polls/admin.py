159c3e142d169fba757c2d826003984a655a7471
Vy Thuy <vng0510@gmail.com>
2017-03-21 00:08:35 -0400

    show all fields of Question obj

--- ui/polls/admin.py
+++ ui/polls/admin.py
@@ -12,6 +12,7 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date Info', {'fields': ['pub_date']}),
     ]
     inlines = [ChoiceInline]
-
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
