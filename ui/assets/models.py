5cc16f40a42e69a334755441949252aadaf8ab6c
Vy Thuy <vng0510@gmail.com>
2017-03-21 22:29:43 -0400

    new migrations for asset class

--- ui/assets/models.py
+++ ui/assets/models.py
@@ -1,5 +1,13 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Asset(models.Model):
+    ASSET_TYPES = (
+        ('Stock', 'st'),
+        ('Equity', 'eq'),
+        ('Commodity', 'cm'),
+    )
+    
+    name = models.CharField(max_length=200)
+    asset_symbol = models.CharField(max_length=10, default='')
+    asset_type = models.CharField(max_length=10, choices=ASSET_TYPES, default='Stock')
+    
