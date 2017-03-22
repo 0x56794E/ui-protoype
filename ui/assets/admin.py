089fe8ed9d0524de5d445ae6da82853677b469a4
Vy Thuy <vng0510@gmail.com>
2017-03-22 00:45:08 -0400

    update asseet models

--- ui/assets/admin.py
+++ ui/assets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Asset
+
 # Register your models here.
+# to make the class editable from admin page
+
+admin.site.register(Asset)
--- ui/assets/models.py
+++ ui/assets/models.py
@@ -10,4 +10,21 @@ class Asset(models.Model):
     name = models.CharField(max_length=200)
     asset_symbol = models.CharField(max_length=10, default='')
     asset_type = models.CharField(max_length=10, choices=ASSET_TYPES, default='Stock')
+
+    # toString equiv
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+        
+class Portfolio(models.Model):
+    name = models.CharField(max_length=200)
+    creation_date = models.DateTimeField('Date created')
+    assets = models.ManyToManyField(Asset)
     
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
