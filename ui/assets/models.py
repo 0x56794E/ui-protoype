b22058ff797f2a76890f2f4f02f840e0a7d88120
Vy Thuy <vng0510@gmail.com>
2017-03-22 04:23:52 -0400

    more changes to models

--- ui/assets/models.py
+++ ui/assets/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+class Portfolio(models.Model):
+    name = models.CharField(max_length=200)
+    creation_date = models.DateTimeField('Date created')
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
 class Asset(models.Model):
     ASSET_TYPES = (
         ('Stock', 'Stock'),
@@ -10,7 +21,8 @@ class Asset(models.Model):
     name = models.CharField(max_length=200)
     asset_symbol = models.CharField(max_length=10, default='')
     asset_type = models.CharField(max_length=10, choices=ASSET_TYPES, default='Stock')
-
+    portfolios = models.ManyToManyField(Portfolio)
+    
     # toString equiv
     def __str__(self):
         return self.name
@@ -18,17 +30,6 @@ class Asset(models.Model):
     class Meta:
         ordering = ('name',)
         
-class Portfolio(models.Model):
-    name = models.CharField(max_length=200)
-    creation_date = models.DateTimeField('Date created')
-    assets = models.ManyToManyField(Asset)
-    
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        ordering = ('name',)
-
 class AssetPortfolio(models.Model):
     asset = models.ForeignKey(Asset)
     portfolio = models.ForeignKey(Portfolio)
