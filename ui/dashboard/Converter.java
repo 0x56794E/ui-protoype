6c338044c6516eaaf8e109a500139a1882f36908
Vy Thuy <vng0510@gmail.com>
2017-04-06 22:57:42 -0400

    quick & dirty converter

--- ui/dashboard/Converter.java
+++ ui/dashboard/Converter.java
@@ -6,8 +6,8 @@ import java.io.File;
 import java.util.stream.Stream;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Set;
-import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Optional;
 
 /**
@@ -21,6 +21,18 @@ import java.util.Optional;
  */
 public class Converter
 {
+    private static class Pair<K, V>
+    {
+        public final K date;
+        public final V price;
+        
+        public Pair(K date, V price)
+        {
+            this.date = date;
+            this.price = price;
+        }
+    }
+    
 	/**
 	 * Cmd input: <input file path> <output file path>
 	 */
@@ -37,6 +49,13 @@ public class Converter
         converter.exec(args[0], args[1]);
 	}
     
+    //HashMap spec the assets 
+    //and their corresp column idx
+    //Key == array idx; Val == val
+    String[] assetNames;
+    
+    //Key: asset name; Value: List of pairs <date, price>
+    private Map<String, List<Pair<String, String>>> result = new HashMap<>();
     
     private void exec (String input, String output)
         throws IOException
@@ -44,14 +63,33 @@ public class Converter
         Stream<String> lines = Files.lines(Paths.get(input));
         
         //Read the header
-        Optional<String> header = lines.findFirst();
+        Optional<String> headerOpt = lines.findFirst();
         
-        //Process the rest
-        lines.forEach(line -> process(line));        
+        if (!headerOpt.isPresent())
+            throw new IllegalArgumentException("Invalid file format");
+        
+        String header = headerOpt.get();
+        assetNames = header.split(","); //Use this as look up maps
+        
+        //Init the map
+        for (int i = 1; i < assetNames.length; ++i)
+            result.put(assetNames[i], new ArrayList<Pair<String, String>>());
+        
+        //Process the stream skipping the header
+        lines.skip(1).forEach(line -> process(line));
     }
 
     private void process(String line)
     {
+        //Format: <date> <price for stock 0> <prc for stock 1> ... <price for stock n>
+        String toks[] = line.split(",");
+        String date = toks[0];
+        
+        for (int i = 1; i < toks.length; ++i)
+        {
+            result.get(assetNames[i]).add(new Pair<String, String>(date, toks[i]));
+        }
+        
         
     }
 	
