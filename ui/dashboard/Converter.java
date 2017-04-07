b0c25b073ac8144b8a0ca6b86da3aeb57ad80b1c
Vy Thuy <vng0510@gmail.com>
2017-04-06 22:22:43 -0400

    stubs

--- ui/dashboard/Converter.java
+++ ui/dashboard/Converter.java
@@ -1,3 +1,14 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.io.File;
+
+import java.util.stream.Stream;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Optional;
 
 /**
  * Convert CSV file to js array of data 
@@ -5,6 +16,7 @@
  * 
  * Input: prices of different assets (Google, Apple, etc stocks and other asset classes)
  * in certain period.
+ *
  * @author Vy Thuy Nguyen
  */
 public class Converter
@@ -12,15 +24,35 @@ public class Converter
 	/**
 	 * Cmd input: <input file path> <output file path>
 	 */
-	public void (String[] args)
+	public static void main (String[] args)
+        throws Exception
 	{
 		if (args.length < 2)
 		{
 			System.out.println("Usage: java Coverter <input file path> <output file path>");
 			System.exit(1);
 		}
-		
-		
+	
+        Converter converter = new Converter();
+        converter.exec(args[0], args[1]);
 	}
+    
+    
+    private void exec (String input, String output)
+        throws IOException
+    {
+        Stream<String> lines = Files.lines(Paths.get(input));
+        
+        //Read the header
+        Optional<String> header = lines.findFirst();
+        
+        //Process the rest
+        lines.forEach(line -> process(line));        
+    }
+
+    private void process(String line)
+    {
+        
+    }
 	
 }
