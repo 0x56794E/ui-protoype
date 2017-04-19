e24e49a8bad38787b2290d604e4182693a21d2b6
Vy Thuy <vng0510@gmail.com>
2017-04-18 23:16:16 -0400

    more prediction charts

--- ui/dashboard/static/data/backtestedreturns.js
+++ ui/dashboard/static/data/backtestedreturns.js
@@ -8,3 +8,15 @@ returnsCharts["GOOGL"] = "<iframe width='450' height='320' frameborder='0' scrol
 returnsCharts["DG"] = "<iframe width='450' height='320' frameborder='0' scrolling='no' src='//plot.ly/~DavidFB/27.embed'></iframe>"
 
 returnsCharts["EXC"] = "<iframe width='450' height='320' frameborder='0' scrolling='no' src='//plot.ly/~DavidFB/29.embed'></iframe>";
+
+returnsCharts["VZ"] = "<iframe width='450' height='320' frameborder='0' scrolling='no' src='//plot.ly/~DavidFB/31.embed'></iframe>";
+
+
+returnsCharts["NEM"] = "<iframe width='450' height='320' frameborder='0' scrolling='no' src='//plot.ly/~DavidFB/33.embed'></iframe>";
+
+
+returnsCharts["CSC"] = "<iframe width='450' height='320' frameborder='0' scrolling='no' src='//plot.ly/~DavidFB/35.embed'></iframe>";
+
+returnsCharts["NVDA"] = "<iframe width='450' height='320' frameborder='0' scrolling='no' src='//plot.ly/~DavidFB/37.embed'></iframe>";
+
+returnsCharts["BATS.L"] = "<iframe width='450' height='320' frameborder='0' scrolling='no' src='//plot.ly/~DavidFB/39.embed'></iframe>";
